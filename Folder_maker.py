fc5dad83ee1a04121f3c266e79db4181daf8cbbd
Felipe <dfzunigah@unal.edu.co>
2023-05-30 11:14:21 -0500

    Add files via upload

---
+++ Folder_maker.py
@@ -0,0 +1,15 @@
+import os
+
+###
+#Swappable URL
+#https://drive.google.com/open?id=1KmUOZsxmQsGIs4DcbgIPPsddlNVBE_UN_DcNh73
+#https://drive.google.com/u/4/uc?export=download&confirm&id=1-aP2XN_56tLe43eoeIHgnSlnpMIaFBzlh3dC_Z2
+
+
+
+list = ['CC_XX1','CC_XX2']
+  
+for items in list:
+    os.mkdir(items)
+
+
